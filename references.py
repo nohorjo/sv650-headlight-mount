9d0ad5581a433c3af0a18eeff920a48586a189ae
Muhammed Haque <nohorjo@gmail.com>
2019-10-29 18:30:33 +0000

    #master - Add debug option

--- references.py
+++ references.py
@@ -12,10 +12,13 @@ def fork():
     
     return model
 
-def indicator_hole():
-    model = cube([10, 26, 4])
+def indicator_hole(_debug = False):
+    model = cube([10, 26, 5])
 
-    return super_hole(model, 'indicator_hole')
+    if _debug:
+        return debug(model)
+    else:
+        return super_hole(model, 'indicator_hole')
 
 if __name__ == '__main__':
     model = fork()
