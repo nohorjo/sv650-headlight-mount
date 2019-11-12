ba8698cf61a1d02bfd9b57b179fc6c66f91ef4c8
Muhammed Haque <nohorjo@gmail.com>
2019-11-12 22:13:18 +0000

    #master - Create headlight screw dampener

---
+++ headlight_screw_dampener.py
@@ -0,0 +1,12 @@
+from solid import *
+from solid.utils import *
+
+from constants import *
+
+if __name__ == '__main__':
+    h: float = 9
+    
+    model = cylinder(h = h, d = 6) - cylinder(h = h, d = screw_d)
+
+    scad_render_to_file(model, '_%s.scad'% __file__[:-3])
+
