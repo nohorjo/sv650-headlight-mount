1273fdbdf98c67c77642109387e2304d6e25844f
Muhammed Haque <nohorjo@gmail.com>
2019-11-21 22:35:54 +0000

    #master - add dash mount lower to bucket headlight join

--- bucket_headlight_join.py
+++ bucket_headlight_join.py
@@ -1,3 +1,4 @@
+import math
 from solid import *
 from solid.utils import *
 
@@ -6,12 +7,13 @@ from super_hole import *
 from references import *
 from chamfer import *
 
-if __name__ == '__main__':
+def bucket_headlight_join():
     outer_r: float = 86.5
     thick: float = 3
     base_h: float = thick + 3
+    inner_r: float = outer_r - (2 * thick)
     model = circle(r = outer_r + thick)
-    model -= circle(r = outer_r - (thick * 2))
+    model -= circle(r = inner_r)
     model = linear_extrude(base_h)(model)
 
     gap = circle(r = outer_r)
@@ -35,6 +37,42 @@ if __name__ == '__main__':
     model += tab
     model += rotate(130, DOWN_VEC)(tab)
     model += rotate(230, DOWN_VEC)(tab)
+
+
+    model = rotate(40, UP_VEC)(model)
+    model = rotate(2 * math.degrees(math.asin(22 / inner_r)), UP_VEC)(model)
+    model = rotate(180, RIGHT_VEC)(model)
+
+    top_arc = lambda rad, bigger = False: arc(rad = rad, start_degrees = -40 - (1 if bigger else 0), end_degrees = 40 + (1 if bigger else 0))
+
+    top_part = top_arc(outer_r + thick)
+    top_part -= top_arc(outer_r - thick - 7, True)
+    top_part -= top_arc(outer_r) - top_arc(outer_r - 7)
+    top_part = linear_extrude(20)(top_part)
+    top_part += down(base_h)(linear_extrude(base_h + thick)(
+        top_arc(inner_r + thick) - top_arc(inner_r - thick - 1, True)
+    ))
+
+    holes = cylinder(h = dash_link_gap, d = screw_d)
+    for i in range(dash_screw_hole_count):
+        holes += right(i * (dash_link_base_x / dash_screw_hole_count))(
+            hole()(cylinder(h = dash_link_gap, d = screw_d))
+        )
+
+    screw_point = cube([30, 17, dash_link_gap - 1])
+    screw_point -= translate([10 / 2, 20 / 2])(holes)
+    screw_point = rotate(90, RIGHT_VEC)(screw_point)
+    screw_point = rotate(90, FORWARD_VEC)(screw_point)
+    screw_point = translate([outer_r, dash_link_gap / 2, 30 - (thick * 2)])(screw_point)
+
+    top_part += screw_point
+
+    model += top_part
+
+    return model
+
+if __name__ == '__main__':
+    model = bucket_headlight_join()
 
     scad_render_to_file(model, '_%s.scad'% __file__[:-3])
 
--- dash_mount.py
+++ dash_mount.py
@@ -7,6 +7,7 @@ from constants import *
 from references import bucket
 from dash_mount_upper import dash_mount_upper, trapezium, lower_plug_d
 from dash_mount_link import dash_mount_link
+from bucket_headlight_join import  bucket_headlight_join
 
 if __name__ == '__main__':
     tilt_angle: float = 60
@@ -23,6 +24,12 @@ if __name__ == '__main__':
     link = translate([-dash_link_thick / 2, 10, 61])(link)
 
     model += link
+
+    model += translate([0, 30, -50])(
+        rotate(90, RIGHT_VEC)(
+            rotate(90, UP_VEC)(bucket_headlight_join())
+        )
+    )
 
     scad_render_to_file(model, '_%s.scad'% __file__[:-3])
 
