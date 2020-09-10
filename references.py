495ad4b94fadfc765e666ca0a4f2ff986fedbe67
Muhammed Haque <nohorjo@gmail.com>
2020-09-10 18:01:47 +0100

    Add rotate options to moveable point

--- references.py
+++ references.py
@@ -1,5 +1,4 @@
-from solid import *
-from solid.utils import *
+import math
 
 from constants import *
 
@@ -7,6 +6,7 @@ class MoveablePoint:
     def __init__(self, x: float = 0, y: float = 0):
         self.x = x
         self.y = y
+        self.angle = 0
 
     def val(self):
         return [self.x, self.y]
@@ -25,5 +25,24 @@ class MoveablePoint:
 
     def right(self, v: float):
         self.x += v
+        return self
+
+    def set(self, x = 0, y = 0, angle = 0):
+        self.x = x
+        self.y = y
+        self.angle = 0
+        return self
+
+    def reset(self):
+        return self.set()
+
+    def rotate(self, degrees):
+        self.angle += degrees
+        self.angle %= 360
+        return self
+
+    def forward(self, v):
+        self.x += v * math.cos(math.radians(self.angle))
+        self.y += v * math.sin(math.radians(self.angle))
         return self
 
